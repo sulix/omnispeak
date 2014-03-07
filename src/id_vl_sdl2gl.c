eb9b166055dc9673043453841bdae9594ff06acb
NY00123 <NY00123@users.noreply.github.com>
2014-03-07 17:45:50 +0200

    A few visual changes can be observed (with the SDL 2.0 backend) in terms of initial window dimensions: - The internal window contents (with no border) have the ratio of 4:3 now. - The initial dimensions are now smaller so the window fits (somewhat) better on lower-res displays (say 1366x768).

--- src/id_vl_sdl2gl.c
+++ src/id_vl_sdl2gl.c
@@ -136,9 +136,17 @@ const char *pxprog = 	"#version 110\n"\
 static void VL_SDL2GL_SetVideoMode(int mode)
 {
 	assert(mode == 0xD);
+	// Here is how the dimensions of the window are currently picked:
+	// 1. The emulated 320x200 sub-window is first zoomed
+	// by a factor of 3 (for each dimension) to 960x600.
+	// 2. The height is then multiplied by 1.2, so the internal contents
+	// (without the borders) have the aspect ratio of 4:3.
+	//
+	// There are a few more tricks in use to handle the overscan border
+	// and VGA line doubling.
 	vl_sdl2gl_window = SDL_CreateWindow("Commander Keen",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-	                                    VL_VGA_GFX_SCALED_WIDTH_PLUS_BORDER*4/VL_VGA_GFX_WIDTH_SCALEFACTOR,
-	                                    VL_VGA_GFX_SCALED_HEIGHT_PLUS_BORDER*4/VL_VGA_GFX_HEIGHT_SCALEFACTOR,
+	                                    VL_VGA_GFX_SCALED_WIDTH_PLUS_BORDER*3/VL_VGA_GFX_WIDTH_SCALEFACTOR,
+	                                    6*VL_VGA_GFX_SCALED_HEIGHT_PLUS_BORDER*3/(5*VL_VGA_GFX_HEIGHT_SCALEFACTOR),
 	                                    SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
 	vl_sdl2gl_context = SDL_GL_CreateContext(vl_sdl2gl_window);
 	vl_sdl2gl_screenWidth = VL_EGAVGA_GFX_WIDTH;
