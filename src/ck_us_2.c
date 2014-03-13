21c9bd1232612b9117d6480b53f089d869f3ebbe
NY00123 <NY00123@users.noreply.github.com>
2014-03-13 13:34:58 +0200

    Fix C++ compilation for Windows

--- src/ck_us_2.c
+++ src/ck_us_2.c
@@ -17,6 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#include <stdlib.h>
 #include "SDL.h"
 
 #include "id_us.h"
