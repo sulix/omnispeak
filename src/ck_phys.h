8c8ff3ac594029943dcd5922392d2f25719be9d3
NY00123 <ny00@outlook.com>
2016-07-22 23:08:17 +0300

    ck_keenIgnoreVertClip fixes

--- src/ck_phys.h
+++ src/ck_phys.h
@@ -56,7 +56,7 @@ typedef struct CK_objPhysDataDelta
 struct CK_object;
 struct CK_action;
 
-bool ck_keenIgnoreVertClip;
+extern bool ck_keenIgnoreVertClip;
 
 bool CK_NotStuckInWall(struct CK_object *obj);
 bool CK_PreviewClipRects(struct CK_object *obj, struct CK_action *act);
