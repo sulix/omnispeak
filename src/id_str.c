da9d5f76100d9e84f988e49ce1c5798773a4f4a8
David Gow <david@ingeniumdigital.com>
2019-07-10 23:32:06 -0700

    Include <ctype.h> in id_str for isspace()
    This was causing warnings on Win32, and compilation failure on DOS.

--- src/id_str.c
+++ src/id_str.c
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "id_mm.h"
 #include "ck_cross.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
