9421c6aff7ded04b0dc2e9f574a45ea0da4a9b75
David Gow <david@ingeniumdigital.com>
2019-07-18 22:22:28 -0700

    Add timer-based fallback for ID_SD_SDL (and /NOAUDIOSYNC)
    Game (and sound) timing for Commander Keen traditionally was determined
    using the PIT (Programmable Interrupt Timer), which causes the
    SDL_t0Service callback to be called at a regular rate.
    
    For Omnispeak's ID_SD_SDL backend, the number of sound samples generated
    is used to drive this timer: this is high-resolution, and guarantees
    that the sound will have accurate timing (useful if the game is being
    recorded).
    
    However, if the SDL audio device can't be started, or if it's broken,
    timing for the whole game will fail. This change adds a fallback using a
    separate timing thread, which will drive the timer independently of the
    rest of the sound system. It is used if the SDL audio device can't be
    opened (where otherwise, the game would hang as no time ever passes),
    and can be forced with the /NOAUDIOSYNC parameter, if there are issues
    with the audio API in use. (I've had some issues with wine and SDL's
    wasapi driver, and this makes the game playable, though the sound is
    still very broken.)
    
    Note that SDL1 lacks the SDL_GetPerformance{Counter,Frequency}()
    functions, and so we fall back to SDL_GetTicks(), which lacks precision.
    Indeed, the precision is low enough, that the time is rounded down,
    causing the game to run noticeably slowly. A warning is printed if this
    fallback is used with SDL1.

--- src/id_sd_sdl.c
+++ src/id_sd_sdl.c
@@ -36,6 +36,7 @@
 #include <string.h>
 
 #include "id_sd.h"
+#include "id_us.h"
 #include "ck_cross.h"
 
 #define PC_PIT_RATE 1193182
@@ -65,6 +66,12 @@ static uint32_t SD_SDL_SampleOffsetInSound, SD_SDL_SamplesInCurrentPart;
 static int16_t SD_ALOut_Samples[512];
 static uint32_t SD_ALOut_SamplesStart = 0, SD_ALOut_SamplesEnd = 0;
 
+// Used for the timer fallback.
+static volatile int SD_SDL_timerDivisor = 1;
+static volatile bool SD_SDL_useTimerFallback = false;
+static uint64_t SD_LastPITTickTime;
+static SDL_Thread *SD_SDL_t0Thread = 0;
+
 /* NEVER call this from the SDL callback!!! (Or you want a deadlock?) */
 void SD_SDL_SetTimer0(int16_t int_8_divisor)
 {
@@ -72,6 +79,9 @@ void SD_SDL_SetTimer0(int16_t int_8_divisor)
 	// Since the following division may lead to truncation, SD_SDL_SamplesInCurrentPart
 	// can change during playback by +-1 (otherwise music may be a bit faster than intended).
 	SD_SDL_SamplesInCurrentPart = SD_SDL_ScaledSamplesPerPartsTimesPITRate / PC_PIT_RATE;
+
+	// For the timer fallback.
+	SD_SDL_timerDivisor = int_8_divisor;
 }
 
 /*******************************************************************************
@@ -196,7 +206,7 @@ void SD_SDL_CallBack(void *unused, Uint8 *stream, int len)
 #endif
 	while (len)
 	{
-		if (!SD_SDL_SampleOffsetInSound)
+		if (!SD_SDL_SampleOffsetInSound && !SD_SDL_useTimerFallback)
 		{
 			SDL_t0Service();
 		}
@@ -241,6 +251,28 @@ void SD_SDL_CallBack(void *unused, Uint8 *stream, int len)
 	}
 }
 
+int SD_SDL_t0InterruptThread(void *param)
+{
+	while (SD_SDL_useTimerFallback)
+	{
+#if SDL_VERSION_ATLEAST(2, 0, 0)
+		uint64_t currPitTicks = (uint64_t)(SDL_GetPerformanceCounter()) * PC_PIT_RATE / SDL_GetPerformanceFrequency();
+#else
+		uint64_t currPitTicks = (uint64_t)(SDL_GetTicks()) * PC_PIT_RATE / 1000;
+#endif
+		uint32_t ticks = (currPitTicks - SD_LastPITTickTime) / SD_SDL_timerDivisor;
+
+		if (ticks)
+		{
+			SDL_LockAudio();
+			SDL_t0Service();
+			SDL_UnlockAudio();
+			SD_LastPITTickTime = currPitTicks;
+		}
+	}
+	return 0;
+}
+
 void SD_SDL_PCSpkOn(bool on, int freq)
 {
 	SD_PC_Speaker_On = on;
@@ -255,11 +287,16 @@ void SDL_ALService(void);
 
 void SD_SDL_Startup(void)
 {
-
+	for (int i = 0; i < us_argc; ++i)
+	{
+		if (!CK_Cross_strcasecmp(us_argv[i], "/NOAUDIOSYNC"))
+			SD_SDL_useTimerFallback = true;
+	}
 	if (SDL_InitSubSystem(SDL_INIT_AUDIO) < 0)
 	{
 		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "SDL audio system initialization failed,\n%s\n", SDL_GetError());
 		SD_SDL_AudioSubsystem_Up = false;
+		SD_SDL_useTimerFallback = true;
 	}
 	else
 	{
@@ -293,6 +330,22 @@ void SD_SDL_Startup(void)
 		}
 		SDL_PauseAudio(0);
 	}
+	// Start the timer fallback if needed.
+	if (SD_SDL_useTimerFallback)
+	{
+#if SDL_VERSION_ATLEAST(2, 0, 0)
+		uint64_t currPitTicks = (uint64_t)(SDL_GetPerformanceCounter()) * PC_PIT_RATE / SDL_GetPerformanceFrequency();
+#else
+		uint64_t currPitTicks = (uint64_t)(SDL_GetTicks()) * PC_PIT_RATE / 1000;
+		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Using SDL sound timer fallback with SDL 1.2: precision issues may make the game run at the wrong speed.\n");
+#endif
+		SD_LastPITTickTime = currPitTicks;
+#if SDL_VERSION_ATLEAST(2, 0, 0)
+		SD_SDL_t0Thread = SDL_CreateThread(SD_SDL_t0InterruptThread, "ID_SD: t0 interrupt thread.", NULL);
+#else
+		SD_SDL_t0Thread = SDL_CreateThread(SD_SDL_t0InterruptThread, NULL);
+#endif
+	}
 }
 
 void SD_SDL_Shutdown(void)
@@ -302,6 +355,11 @@ void SD_SDL_Shutdown(void)
 		SDL_CloseAudio();
 		SDL_QuitSubSystem(SDL_INIT_AUDIO);
 		SD_SDL_AudioSubsystem_Up = false;
+	}
+	if (SD_SDL_useTimerFallback)
+	{
+		SD_SDL_useTimerFallback = false;
+		SDL_WaitThread(SD_SDL_t0Thread, NULL);
 	}
 }
 
