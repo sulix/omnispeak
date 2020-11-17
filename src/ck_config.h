8d6ef2eaa36753438f18bee4571f24aabaea4601
David Gow <david@ingeniumdigital.com>
2020-11-17 16:54:10 +0800

    id_ti: Handle out-of-bounds tileinfo accesses (Issue #28)
    In some circumstances, when using a glitch to read past the bounds of a
    level, invalid tileinfo data can be read. This reads potentially invalid
    memory, and can therefore cause a crash.
    
    With this change, all out-of-bounds tileinfo access returns 0. The
    glitches still appear to function as expected in DOS with this change,
    but without crashing. See Issue #28 for more details.
    
    Note that in debug builds, we log a warning for each out-of-bounds
    tileinfo access. This spams a lot of log messages in the case of this
    glitch (when level 2 is played after level 1), so may slow the game down
    a bit.

--- src/ck_config.h
+++ src/ck_config.h
@@ -39,7 +39,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #ifdef CK_DEBUG
 // Debug version of the ID_MM_Arena allocator.
 #define ID_MM_DEBUGARENA
-
+// Warn on out-of-bounds access to tileinfo.
+#define CK_WARN_ON_TILEINFO_OOB
 #endif
 
 // =================================
--- src/id_ti.c
+++ src/id_ti.c
@@ -17,9 +17,17 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#include "ck_config.h"
+#include "ck_cross.h"
 #include "id_ca.h"
 
 #include <stdint.h>
+
+#ifdef CK_WARN_ON_TILEINFO_OOB
+#define TI_WARN_ON_OOB() CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Out of bounds tileinfo access in %s (tile = %d, $%x)\n", __PRETTY_FUNCTION__, tile, tile)
+#else
+#define TI_WARN_ON_OOB() do { } while (0)
+#endif
 
 // Tileinfo
 
@@ -29,52 +37,97 @@ uint8_t *ti_tileInfo;
 int8_t TI_BackAnimTile(uint16_t tile)
 {
 	const int index = tile + ca_gfxInfoE.numTiles16;
+	if (tile > ca_gfxInfoE.numTiles16)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[index];
 }
 
 uint8_t TI_BackAnimTime(uint16_t tile)
 {
+	if (tile > ca_gfxInfoE.numTiles16)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[tile];
 }
 
 uint8_t TI_ForeTop(uint16_t tile)
 {
 	const int index = tile + ca_gfxInfoE.numTiles16 * 2;
+	if (tile > ca_gfxInfoE.numTiles16m)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[index];
 }
 
 uint8_t TI_ForeBottom(uint16_t tile)
 {
 	const int index = tile + ca_gfxInfoE.numTiles16 * 2 + ca_gfxInfoE.numTiles16m * 2;
+	if (tile > ca_gfxInfoE.numTiles16m)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[index];
 }
 
 uint8_t TI_ForeLeft(uint16_t tile)
 {
 	const int index = tile + ca_gfxInfoE.numTiles16 * 2 + ca_gfxInfoE.numTiles16m * 3;
+	if (tile > ca_gfxInfoE.numTiles16m)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[index];
 }
 
 uint8_t TI_ForeRight(uint16_t tile)
 {
 	const int index = tile + ca_gfxInfoE.numTiles16 * 2 + ca_gfxInfoE.numTiles16m;
+	if (tile > ca_gfxInfoE.numTiles16m)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[index];
 }
 
 uint8_t TI_ForeMisc(uint16_t tile)
 {
 	const int index = tile + ca_gfxInfoE.numTiles16 * 2 + ca_gfxInfoE.numTiles16m * 5;
+	if (tile > ca_gfxInfoE.numTiles16m)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[index];
 }
 
 int8_t TI_ForeAnimTile(uint16_t tile)
 {
 	const int index = tile + ca_gfxInfoE.numTiles16 * 2 + ca_gfxInfoE.numTiles16m * 4;
+	if (tile > ca_gfxInfoE.numTiles16m)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[index];
 }
 
 uint8_t TI_ForeAnimTime(uint16_t tile)
 {
 	const int index = tile + ca_gfxInfoE.numTiles16 * 2 + ca_gfxInfoE.numTiles16m * 6;
+	if (tile > ca_gfxInfoE.numTiles16m)
+	{
+		TI_WARN_ON_OOB();
+		return 0;
+	}
 	return ti_tileInfo[index];
 }
