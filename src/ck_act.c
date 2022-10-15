sulix/omnispeak

commit e24e525b2b23e93c8533110ba874dbe0e038ba9e

id_str: Get rid of TempArena
Ding, dong, the witch is dead!

Finally, the ugly hack which was the TempArena in STR_ParserState is
gone. This means that the STR_GetString() and STR_GetIdent() functions
now take a pointer to a buffer into which to write the value. This is a
big uglier to use, but allows us to avoid several copies of the string,
which is great for both memory usage and wasted CPU cycles.

Exprect huge stack sizes from some functions, though, particularly with
such a big maximum token size.

src/ck_act.c
@@ -284,30 +284,31 @@ CK_VAR_VarType CK_VAR_ParseVarType(STR_ParserState *ps)
 
 CK_ActionType CK_ACT_GetActionType(STR_ParserState *ps)
 {
-	const char *tok = STR_GetIdent(ps);
+	STR_Token tok = STR_GetToken(ps);
 	CK_ActionType at = AT_UnscaledOnce;
 
-	if (!strcmp(tok, "UnscaledOnce"))
+	if (STR_IsTokenIdent(tok, "UnscaledOnce"))
 		at = AT_UnscaledOnce;
-	else if (!strcmp(tok, "ScaledOnce"))
+	else if (STR_IsTokenIdent(tok, "ScaledOnce"))
 		at = AT_ScaledOnce;
-	else if (!strcmp(tok, "Frame"))
+	else if (STR_IsTokenIdent(tok, "Frame"))
 		at = AT_Frame;
-	else if (!strcmp(tok, "UnscaledFrame"))
+	else if (STR_IsTokenIdent(tok, "UnscaledFrame"))
 		at = AT_UnscaledFrame;
-	else if (!strcmp(tok, "ScaledFrame"))
+	else if (STR_IsTokenIdent(tok, "ScaledFrame"))
 		at = AT_ScaledFrame;
 	else
 	{
 		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Got a bad action type %s on line %d.\n", tok, ps->linecount);
-		at = (CK_ActionType)(atoi(tok));
+		at = (CK_ActionType)(STR_GetIntegerValue(tok));
 	}
 	return at;
 }
 
 bool CK_VAR_ParseAction(STR_ParserState *ps)
 {
-	const char *actName = STR_GetIdent(ps);
+	char actName[ID_STR_MAX_TOKEN_LENGTH];
+	STR_GetIdent(ps, actName, ID_STR_MAX_TOKEN_LENGTH);
 
 	CK_action *act = CK_GetOrCreateActionByName(actName);
 
@@ -328,16 +329,19 @@ bool CK_VAR_ParseAction(STR_ParserState *ps)
 	act->velX = STR_GetInteger(ps);
 	act->velY = STR_GetInteger(ps);
 
-	const char *cThink, *cCollide, *cDraw;
-	cThink = STR_GetIdent(ps);
-	cCollide = STR_GetIdent(ps);
-	cDraw = STR_GetIdent(ps);
+	char cThink[ID_STR_MAX_TOKEN_LENGTH];
+	char cCollide[ID_STR_MAX_TOKEN_LENGTH];
+	char cDraw[ID_STR_MAX_TOKEN_LENGTH];
+	STR_GetIdent(ps, cThink, sizeof(cThink));
+	STR_GetIdent(ps, cCollide, sizeof(cCollide));
+	STR_GetIdent(ps, cDraw, sizeof(cDraw));
 
 	act->think = CK_ACT_GetFunction(cThink);
 	act->collide = CK_ACT_GetColFunction(cCollide);
 	act->draw = CK_ACT_GetFunction(cDraw);
 
-	const char *nextActionName = STR_GetIdent(ps);
+	char nextActionName[ID_STR_MAX_TOKEN_LENGTH];
+	STR_GetIdent(ps, nextActionName, ID_STR_MAX_TOKEN_LENGTH);
 
 	act->next = strcmp(nextActionName, "NULL") ? CK_GetOrCreateActionByName(nextActionName) : 0;
 
@@ -346,19 +350,27 @@ bool CK_VAR_ParseAction(STR_ParserState *ps)
 
 void CK_VAR_ParseInt(STR_ParserState *ps)
 {
-	const char *varName = STR_GetIdent(ps);
+	char varName[ID_STR_MAX_TOKEN_LENGTH];
+	STR_GetIdent(ps, varName, ID_STR_MAX_TOKEN_LENGTH);
 	intptr_t val = STR_GetInteger(ps);
 	CK_VAR_SetInt(varName, val);
 }
 
 void CK_VAR_ParseString(STR_ParserState *ps)
 {
-	const char *varName = STR_GetIdent(ps);
+	char varName[ID_STR_MAX_TOKEN_LENGTH];
+	STR_GetIdent(ps, varName, ID_STR_MAX_TOKEN_LENGTH);
 	char stringBuf[4096] = {0};
+	char *valBuf = stringBuf;
+	size_t lengthRemaining = 4096;
 	do
 	{
-		const char *val = STR_GetString(ps);
-		strcat(stringBuf, val);
+		STR_Token tok = STR_GetToken(ps);
+		size_t chunkLen = STR_GetStringValue(tok, valBuf, lengthRemaining);
+		lengthRemaining -= chunkLen;
+		valBuf += chunkLen;
+		if (!lengthRemaining)
+			Quit("String too long!");
 	} while (STR_PeekToken(ps).tokenType == STR_TOK_String);
 	CK_VAR_SetString(varName, stringBuf);
 }
@@ -369,7 +381,6 @@ bool CK_VAR_ParseVar(STR_ParserState *ps)
 
 	if (varType == VAR_EOF)
 	{
-		//MM_ArenaReset(ps->tempArena);
 		return false;
 	}
 
@@ -386,7 +397,8 @@ bool CK_VAR_ParseVar(STR_ParserState *ps)
 		break;
 	case VAR_TOK_Include:
 	{
-		const char *filename = STR_GetString(ps);
+		char filename[ID_STR_MAX_TOKEN_LENGTH];
+		STR_GetString(ps, filename, ID_STR_MAX_TOKEN_LENGTH);
 		CK_VAR_LoadVars(filename);
 		break;
 	}
@@ -394,7 +406,6 @@ bool CK_VAR_ParseVar(STR_ParserState *ps)
 		Quit("Unsupported var type.");
 	}
 
-	//MM_ArenaReset(ps->tempArena);
 	return true;
 }
 
@@ -408,12 +419,8 @@ void CK_VAR_LoadVars(const char *filename)
 	parserstate.linecount = 0;
 	parserstate.haveBufferedToken = false;
 
-	parserstate.tempArena = MM_ArenaCreate(65536);
-
 	while (CK_VAR_ParseVar(&parserstate))
 		numVarsParsed++;
 
 	CK_Cross_LogMessage(CK_LOG_MSG_NORMAL, "Parsed %d vars from \"%s\" over %d lines (%d actions created).\n", numVarsParsed, filename, parserstate.linecount, ck_actionsUsed);
-
-	MM_ArenaDestroy(parserstate.tempArena);
 }

src/id_cfg.c
@@ -121,30 +121,28 @@ void CFG_SetConfigEnum(const char *name, const char **strings, int value)
 
 static bool CFG_ParseConfigLine(STR_ParserState *state)
 {
-	const char *name = STR_GetIdent(state);
-	if (!name)
+	STR_Token nameTok = STR_GetToken(state);
+	if (nameTok.tokenType == STR_TOK_EOF)
 		return false;
+	char name[ID_STR_MAX_TOKEN_LENGTH];
+	STR_GetStringValue(nameTok, name, ID_STR_MAX_TOKEN_LENGTH);
 	if (!STR_ExpectToken(state, "="))
 		return false;
-	STR_Token value = STR_PeekToken(state);
+	STR_Token value = STR_GetToken(state);
 	if (value.tokenType == STR_TOK_EOF)
 		return false;
 	else if (value.tokenType == STR_TOK_String)
-		CFG_SetConfigString(name, STR_GetString(state));
+	{
+		char tokenBuf[ID_STR_MAX_TOKEN_LENGTH];
+		STR_GetStringValue(value, tokenBuf, ID_STR_MAX_TOKEN_LENGTH);
+		CFG_SetConfigString(name, tokenBuf);
+	}
 	else if (STR_IsTokenIdentCase(value, "false"))
-	{
 		CFG_SetConfigBool(name, false);
-		// Eat the token.
-		STR_GetToken(state);
-	}
 	else if (STR_IsTokenIdentCase(value, "true"))
-	{
 		CFG_SetConfigBool(name, true);
-		// Eat the token.
-		STR_GetToken(state);
-	}
 	else
-		CFG_SetConfigInt(name, STR_GetInteger(state));
+		CFG_SetConfigInt(name, STR_GetIntegerValue(value));
 	return true;
 }
 
@@ -161,14 +159,11 @@ void CFG_LoadConfig(const char *filename)
 	parserstate.linecount = 0;
 	parserstate.haveBufferedToken = false;
 
-	parserstate.tempArena = MM_ArenaCreate(4096);
-
 	while (CFG_ParseConfigLine(&parserstate))
 		numVarsParsed++;
 
 	CK_Cross_LogMessage(CK_LOG_MSG_NORMAL, "Parsed %d config options from \"%s\" over %d lines\n", numVarsParsed, filename, parserstate.linecount);
 
-	MM_ArenaDestroy(parserstate.tempArena);
 	MM_FreePtr((mm_ptr_t *)&parserstate.data);
 }
 
@@ -188,11 +183,12 @@ static bool CFG_SaveConfigLine(FS_File outputHandle, STR_ParserState *state)
 	// We update the "unmodified range" so that its end is the beginning
 	// of the name (which is an identifier).
 	//
-	STR_Token nameTok = STR_PeekToken(state);
+	char name[ID_STR_MAX_TOKEN_LENGTH];
+	STR_Token nameTok = STR_GetToken(state);
 	cfg_unmodifiedRangeEnd = nameTok.firstIndex;
-	const char *name = STR_GetString(state);
-	if (!name)
+	if (nameTok.tokenType == STR_TOK_EOF)
 		return false;
+	STR_GetStringValue(nameTok, name, ID_STR_MAX_TOKEN_LENGTH);
 	if (!STR_ExpectToken(state, "="))
 	{
 		// If we're at the end of the file, the unmodified range should
@@ -314,15 +310,12 @@ void CFG_SaveConfig(const char *filename)
 		parserstate.linecount = 0;
 		parserstate.haveBufferedToken = false;
 
-		parserstate.tempArena = MM_ArenaCreate(4096);
-
 		// Go through the file, re-writing the values of all the config vars.
 		while (CFG_SaveConfigLine(outputHandle, &parserstate))
 			numVarsSaved++;
 
 		CFG_FlushUnmodifiedRange(outputHandle, &parserstate);
 
-		MM_ArenaDestroy(parserstate.tempArena);
 		MM_FreePtr((mm_ptr_t *)&parserstate.data);
 	}
 

src/id_str.c
@@ -220,13 +220,12 @@ STR_Token STR_PeekToken(STR_ParserState *ps)
 }
 
 /* Parses the string value out of a token, storing the result in memory from destArena */
-const char *STR_GetStringValue(STR_Token tok, ID_MM_Arena *destArena)
+size_t STR_GetStringValue(STR_Token tok, char *tokenBuf, size_t bufLength)
 {
-	char tokenbuf[ID_STR_MAX_TOKEN_LENGTH];
 	int i = 0;
 	
 	if (tok.tokenType == STR_TOK_EOF)
-		return NULL;
+		return 0;
 	
 	if (tok.tokenType == STR_TOK_String)
 	{
@@ -247,8 +246,8 @@ const char *STR_GetStringValue(STR_Token tok, ID_MM_Arena *destArena)
 					break;
 				}
 			}
-			tokenbuf[i++] = c;
-			if (i == ID_STR_MAX_TOKEN_LENGTH)
+			tokenBuf[i++] = c;
+			if (i == bufLength)
 				Quit("Token exceeded max length!");
 		}
 	}
@@ -256,29 +255,26 @@ const char *STR_GetStringValue(STR_Token tok, ID_MM_Arena *destArena)
 	{
 		while (*(tok.valuePtr) && !isspace(*(tok.valuePtr)))
 		{
-			tokenbuf[i++] = *(tok.valuePtr++);
+			tokenBuf[i++] = *(tok.valuePtr++);
 			if (i == ID_STR_MAX_TOKEN_LENGTH)
 				Quit("Token exceeded max length!");
 		}
 	}
-	tokenbuf[i] = '\0';
+	tokenBuf[i] = '\0';
 
-	const char *value = MM_ArenaStrDup(destArena, tokenbuf);
-	return value;
+	return i;
 }
 
-const char *STR_GetString(STR_ParserState *ps)
+size_t STR_GetString(STR_ParserState *ps, char *tokenBuf, size_t bufLength)
 {
 	STR_Token tok = STR_GetToken(ps);
-	//TODO: Get rid of tempArena?
-	return STR_GetStringValue(tok, ps->tempArena);
+	STR_GetStringValue(tok, tokenBuf, bufLength);
 }
 
-const char *STR_GetIdent(STR_ParserState *ps)
+size_t STR_GetIdent(STR_ParserState *ps, char *tokenBuf, size_t bufLength)
 {
 	STR_Token tok = STR_GetToken(ps);
-	//TODO: Split this out from strings.
-	return STR_GetStringValue(tok, ps->tempArena);
+	STR_GetStringValue(tok, tokenBuf, bufLength);
 }
 
 bool STR_IsTokenIdent(STR_Token tok, const char *str)

src/id_str.h
@@ -75,7 +75,6 @@ typedef struct STR_ParserState
 	int dataindex;
 	int datasize;
 	int linecount;
-	ID_MM_Arena *tempArena;
 	// This is a token whose value has been PeekToken()ed.
 	bool haveBufferedToken;
 	STR_Token bufferedToken;
@@ -83,9 +82,9 @@ typedef struct STR_ParserState
 
 STR_Token STR_GetToken(STR_ParserState *ps);
 STR_Token STR_PeekToken(STR_ParserState *ps);
-const char *STR_GetStringValue(STR_Token tok, ID_MM_Arena *destArena);
-const char *STR_GetString(STR_ParserState *ps);
-const char *STR_GetIdent(STR_ParserState *ps);
+size_t STR_GetStringValue(STR_Token tok, char *tokenBuf, size_t bufLen);
+size_t STR_GetString(STR_ParserState *ps, char *tokenBuf, size_t bufLen);
+size_t STR_GetIdent(STR_ParserState *ps, char *tokenBuf, size_t bufLen);
 bool STR_IsTokenIdent(STR_Token tok, const char *str);
 bool STR_IsTokenIdentCase(STR_Token tok, const char *str);
 int STR_GetIntegerValue(STR_Token tok);

commit 81a0779ea6764ad9568cd2f6eb04728194cac6b0

ck_act: Fix a type error when printing an unknown type error.
When we were removing the TempArena, we missed a bit when parsing a
variable type. This meant that were were trying to print 'tok', a
STR_Token, instead of its string value, which we now must allocate a
temporary for and 'get'.

Fixes: e24e525b2b23 ("id_str: Get rid of TempArena")

src/ck_act.c
@@ -299,7 +299,9 @@ CK_ActionType CK_ACT_GetActionType(STR_ParserState *ps)
 		at = AT_ScaledFrame;
 	else
 	{
-		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Got a bad action type %s on line %d.\n", tok, ps->linecount);
+		char varTypeString[ID_STR_MAX_TOKEN_LENGTH];
+		STR_GetStringValue(tok, varTypeString, ID_STR_MAX_TOKEN_LENGTH);
+		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Got a bad action type %s on line %d.\n", varTypeString, ps->linecount);
 		at = (CK_ActionType)(STR_GetIntegerValue(tok));
 	}
 	return at;