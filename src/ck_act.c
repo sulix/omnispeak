sulix/omnispeak

commit 3cdb5734724b45ebd1f8829484b5631ed3b510fd

ck_act: Fix printf warning on 32-bit builds
Because integer variables are of the type intptr_t, which is different
on 32-bit and 64-bit systems, we need to use the magic printf macros to
print it (for the debug warnings).

We also need to include inttypes.h, so replce the unused ctypes.h header.

Otherwise:
ck_act.c: In function ‘CK_VAR_GetInt’:
ck_act.c:170:3: warning: format ‘%ld’ expects argument of type ‘long int’, but argument 4 has type ‘intptr_t’ [-Wformat=]
   CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Integer variable \"%s\" not set, returning default %ld (0x%lX)\n", name, def, def);
   ^
ck_act.c:170:3: warning: format ‘%lX’ expects argument of type ‘long unsigned int’, but argument 5 has type ‘intptr_t’ [-Wformat=]

src/ck_act.c
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "id_str.h"
 #include "id_us.h"
 
-#include <ctype.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "ck_act.h"
@@ -167,7 +167,7 @@ intptr_t CK_VAR_GetInt(const char *name, intptr_t def)
 	if (!var)
 	{
 #ifdef CK_VAR_WARNONNOTSET
-		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Integer variable \"%s\" not set, returning default %ld (0x%lX)\n", name, def, def);
+		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Integer variable \"%s\" not set, returning default %"PRIdPTR" (0x%"PRIXPTR")\n", name, def, def);
 #endif
 		return def;
 	}

commit 7be779941f87647c8fad3c33b84464b802cf1188

ck_act: Fix another printf warning
The previous attempt to fix the intptr_t printf warning actually caused
more problems, as in some newer versions of C++, a space is required
after a string literal before a preprocessor token.

Add the spaces.

Fixes: 3cdb5734724b ("ck_act: Fix printf warning on 32-bit builds")

src/ck_act.c
@@ -167,7 +167,7 @@ intptr_t CK_VAR_GetInt(const char *name, intptr_t def)
 	if (!var)
 	{
 #ifdef CK_VAR_WARNONNOTSET
-		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Integer variable \"%s\" not set, returning default %"PRIdPTR" (0x%"PRIXPTR")\n", name, def, def);
+		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Integer variable \"%s\" not set, returning default %" PRIdPTR " (0x%" PRIXPTR ")\n", name, def, def);
 #endif
 		return def;
 	}