sulix/omnispeak

commit f24c41b1ab98e476956dda984b2fba48154b9c58

ck_keen: Make the "Easy" jump gravity optional
Allow mods to disable the lower-gravity jumps on Easy difficulty from
EPISODE.CKx.

The new ck_easyJumpDifficulty integer variable controls this.
1 (default) enables it, like the original game,
0 disables it, like some mods.

Equivalent to:
%patch $b97d $eb

in Keen 5 v1.4

src/ck_keen.c
@@ -1092,8 +1092,7 @@ void CK_KeenJumpThink(CK_object *obj)
 	}
 	else
 	{
-		//TODO: Check this w/ K5Disasm and/or x-disasm
-		if (ck_gameState.difficulty == D_Easy)
+		if (ck_gameState.difficulty == D_Easy && CK_INT(ck_easyJumpGravity, 1))
 		{
 			CK_PhysGravityMid(obj);
 		}

data/keen4/EPISODE.CK4
@@ -50,6 +50,7 @@
 
 %int ck_worldMapNumber 0
 %function ck_mapMiscFlagsCheck CK4_MapMiscFlagsCheck
+%int ck_easyJumpGravity 1 # Keen jumps higher on "Easy" difficulty.
 
 # Offsets into the .exe for savegame compatability
 %int ck_exe_objArrayOffset 0xA807

data/keen5/EPISODE.CK5
@@ -50,6 +50,7 @@
 
 %int ck_worldMapNumber 0
 %function ck_mapMiscFlagsCheck CK5_MapMiscFlagsCheck
+%int ck_easyJumpGravity 1 # Keen jumps higher on "Easy" difficulty.
 
 # Offsets into the .exe for savegame compatability
 %int ck_exe_objArrayOffset 0x9E6F

data/keen6e14/EPISODE.CK6
@@ -50,6 +50,7 @@
 
 %int ck_worldMapNumber 0
 %function ck_mapMiscFlagsCheck CK6_MapMiscFlagsCheck
+%int ck_easyJumpGravity 1 # Keen jumps higher on "Easy" difficulty.
 
 # Offsets into the .exe for savegame compatability
 # Note these offsets are for version 1.4

data/keen6e15/EPISODE.CK6
@@ -50,6 +50,7 @@
 
 %int ck_worldMapNumber 0
 %function ck_mapMiscFlagsCheck CK6_MapMiscFlagsCheck
+%int ck_easyJumpGravity 1 # Keen jumps higher on "Easy" difficulty.
 
 # Offsets into the .exe for savegame compatability
 # Note these offsets are for version 1.5

commit ca494ad1a1afda88dbef8bb481f28fe30028cb2f

ck_keen: The Easy Jump gravity toggle should also affect Pogo
Whoops, missed this in the last change — the gravity used on easy should
also affect the Pogo stick.

Thanks NY00123 for noticing!

Fixes: f24c41b1ab98 ("ck_keen: Make the "Easy" jump gravity optional")

src/ck_keen.c
@@ -1339,7 +1339,7 @@ void CK_KeenPogoThink(CK_object *obj)
 {
 	if (!ck_keenState.jumpTimer)
 	{
-		if (ck_gameState.difficulty == D_Easy)
+		if (ck_gameState.difficulty == D_Easy && CK_INT(ck_easyJumpGravity, 1))
 		{
 			CK_PhysGravityMid(obj);
 		}