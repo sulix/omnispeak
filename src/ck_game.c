sulix/omnispeak

commit 49a5f6160331460c5996d15087ada17e182240e3

Abiathar '.aslev' support
This is a quick implementation of loading levels from Abiathar .aslev
files. The bulk of it is an implementation of the Keen1 LZW algorithm.
This implementation is a bit different from most of the existing ones
(the 'dictionary' stores references to the original decompressed data),
but it should work pretty well — there's definitely the possibility of
growing the dictionary dynamically, though, which might be necessary to
support the absolutely huge potential dictionary sizes in .aslev files
(24 bit codes!)

It also loads .aslev files as 'Omni' files, and so they must be located
int the "omni" directory (alongside the game header files, like EGAHEAD,
etc.)

To use this, run Omnispeak with the /ASLEV [filename] option. This is
analagous to the /TEDLEVEL option, but loads the level (as level 1) from
the provided .aslev file.

The two issues (already mentioned above) are:
- .aslev files must live in the 'omni' directory, al;ongside EGAHEAD, etc.
- Loading .aslev files requires a lot of memory (256MB) to store the LZW
  dictionary, and so likely will not work on lower-end systems (e.g. DOS).

Both of those issues may be fixed in future versions... we'll see.

In the meantime, enjoy!

src/ck_game.c
@@ -601,7 +601,10 @@ void CK_LoadLevel(bool doCache, bool silent)
 		US_InitRndT(true);
 	}
 
-	CA_CacheMap(ck_gameState.currentLevel);
+	if (us_aslevFilename)
+		CA_CacheMapFromASLEV(us_aslevFilename);
+	else
+		CA_CacheMap(ck_gameState.currentLevel);
 	RF_NewMap();
 	CA_ClearMarks();
 

src/id_ca.c
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "id_ca.h"
 #include "id_fs.h"
+#include "id_mm.h"
 #include "id_us.h"
 #include "id_vh.h"
 #include "ck_cross.h"
@@ -253,6 +254,135 @@ void CAL_HuffExpand(void *src, void *dest, int expLength, ca_huffnode *table, in
 		}
 	}
 }
+
+#ifndef VANILLA
+
+typedef struct CA_LZW_DictEntry
+{
+	int byteLen;
+	uint8_t *firstByte;
+} CA_LZW_DictEntry;
+
+uint32_t CAL_ReadLZWBits(uint8_t **srcPtr, int *bitOfs, int numBits)
+{
+#ifdef _DEBUG
+	if (numBits > 32)
+		QuitF("CAL_ReadLZWBits: tried to read too many bits (%d > 32)!", numBits);
+#endif
+	int bitsRead = 0;
+	uint32_t bitBuffer = 0;
+	while (bitsRead < numBits)
+	{
+		int bitsToRead = CK_Cross_min(numBits - bitsRead, *bitOfs);
+		uint8_t mask = ((1 << bitsToRead) - 1) << (*bitOfs - bitsToRead);
+		uint8_t val = ((**srcPtr) & mask) >> (*bitOfs - bitsToRead);
+
+
+		bitBuffer = (bitBuffer << bitsToRead) | (uint32_t)val;
+
+		if (bitsToRead == *bitOfs)
+		{
+			*bitOfs = 8;
+			(*srcPtr)++;
+		}
+		else
+		{
+			*bitOfs -= bitsToRead;
+		}
+		bitsRead += bitsToRead;
+	}
+	return bitBuffer;
+}
+
+#define CA_LZW_SPECIAL_ENTRIES 258
+#define CA_LZW_ERROR 256
+#define CA_LZW_EOF 257
+#define CA_LZW_LAST_LITERAL 255
+
+void CAL_LZWExpand(void *src, void *dest, int srcLength, int dictBits)
+{
+	// The first 258 codes are reserved for literals (0–255), error (256), and EOF (257)
+	int numDictEntries = (1 << dictBits) - CA_LZW_SPECIAL_ENTRIES;
+	CA_LZW_DictEntry *lzwDict;
+
+	// Note that this could be _very_ big: *.aslev files have up to 24-bit dict
+	// entries, so need 256 _megs_ of space for a full dictionary. We probably
+	// should grow this lazily.
+	MM_GetPtr((mm_ptr_t *)&lzwDict, numDictEntries * sizeof(CA_LZW_DictEntry));
+	MM_SetLock((mm_ptr_t *)&lzwDict, true);
+
+	int maxDictEntry = CA_LZW_SPECIAL_ENTRIES;
+	uint32_t code = CA_LZW_ERROR;
+	int bitOfs = 8;
+	int codeLen = 9;
+	int prevMatchLen = 1;
+	uint32_t prevCode = CA_LZW_ERROR;
+	uint8_t *dPtr = (uint8_t*)dest;
+	while (code != 257)
+	{
+		int matchLen = 1;
+		code = CAL_ReadLZWBits((uint8_t**)&src, &bitOfs, codeLen);
+		if (code <= CA_LZW_LAST_LITERAL)
+			*dPtr++ = (uint8_t)code;		// Literal byte
+		else if (code == CA_LZW_ERROR)			// Error
+			Quit("CAL_LZWExpand: Error code in compressed data!");
+		else if (code == CA_LZW_EOF)			// EOF
+			break;
+		else if (code < maxDictEntry)		// Existing dictionary entry
+		{
+			matchLen = lzwDict[code-CA_LZW_SPECIAL_ENTRIES].byteLen;
+			for (int i = 0; i < matchLen; ++i)
+			{
+				*dPtr++ = lzwDict[code-CA_LZW_SPECIAL_ENTRIES].firstByte[i];
+			}
+		}
+		else
+		{
+			if (code != maxDictEntry)
+				QuitF("CAL_LZWExpand: Expected code < %d, got %d!", maxDictEntry, code);
+
+			// The first unused dictionary entry consists of the previously-decoded entry, plus the first character
+			// of that entry, repeated.
+			if (prevCode < CA_LZW_SPECIAL_ENTRIES)
+			{
+				matchLen = 1;
+				*dPtr++ = (uint8_t)prevCode;
+				*dPtr++ = (uint8_t)prevCode;
+			}
+			else
+			{
+				matchLen = lzwDict[prevCode-CA_LZW_SPECIAL_ENTRIES].byteLen;
+				for (int i = 0; i < matchLen; ++i)
+				{
+					*dPtr++ = lzwDict[prevCode-CA_LZW_SPECIAL_ENTRIES].firstByte[i];
+				}
+				*dPtr++ = lzwDict[prevCode-CA_LZW_SPECIAL_ENTRIES].firstByte[0];
+			}
+			matchLen++;
+			if (prevMatchLen + 1 != matchLen)
+				QuitF("CAL_LZWExpand: prevMatch + 1 (%d) should match match (%d), decoding %d", prevMatchLen + 1, matchLen, code);
+		}
+
+		// Add the new code to the dict.
+		if (prevCode != CA_LZW_ERROR && maxDictEntry < (1<<dictBits))
+		{
+			// The new entry consists of the last entry, plus the first byte of the current entry.
+			lzwDict[maxDictEntry - CA_LZW_SPECIAL_ENTRIES].byteLen = prevMatchLen + 1;
+			lzwDict[maxDictEntry - CA_LZW_SPECIAL_ENTRIES].firstByte = dPtr - matchLen - prevMatchLen;
+
+
+			maxDictEntry++;
+			if (maxDictEntry >= (1 << codeLen) - 1 && codeLen != dictBits)
+				codeLen++;
+		}
+		prevMatchLen = matchLen;
+		prevCode = code;
+	}
+
+	MM_FreePtr((mm_ptr_t *)&lzwDict);
+}
+
+#endif
 
 #define CA_CARMACK_NEARTAG 0xA700
 #define CA_CARMACK_FARTAG 0xA800
@@ -1157,6 +1287,72 @@ void CA_CacheMap(int mapIndex)
 		MM_FreePtr((void **)(&rlewBuffer));
 	}
 }
+
+// Cache a map from an Abiathar .aslev file.
+void CA_CacheMapFromASLEV(const char *fname)
+{
+	//Unload the previous map.
+	for (int plane = 0; plane < CA_NUMMAPPLANES; ++plane)
+	{
+		if (CA_mapPlanes[plane])
+		{
+			MM_FreePtr((void **)(&CA_mapPlanes[plane]));
+		}
+	}
+
+	uint8_t *aslevFile;
+	int aslevFileLen;
+
+	if (!CA_LoadFile(fname, (void **)(&aslevFile), &aslevFileLen))
+		QuitF("Couldn't load map file %s\n", fname);
+
+	//Have we loaded this map's header?
+	if (CA_MapHeaders[ca_mapOn])
+	{
+		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Tried to override an already-loaded map (%d) with a .aslev!", ca_mapOn);
+		MM_FreePtr((void **)(&CA_MapHeaders[ca_mapOn]));
+	}
+	MM_GetPtr((void **)(&CA_MapHeaders[ca_mapOn]), sizeof(CA_MapHeader));
+
+	memcpy(CA_MapHeaders[ca_mapOn]->name, &aslevFile[12], 16);
+	memcpy(CA_MapHeaders[ca_mapOn]->signature, &aslevFile[8], 16);
+	memcpy(&CA_MapHeaders[ca_mapOn]->width, &aslevFile[28], 2);
+	memcpy(&CA_MapHeaders[ca_mapOn]->height, &aslevFile[30], 2);
+
+	#ifdef CK_CROSS_IS_BIGENDIAN
+	CA_MapHeaders[ca_mapOn]->width = CK_Cross_SwapLE16(CA_MapHeaders[ca_mapOn]->width);
+	CA_MapHeaders[ca_mapOn]->height = CK_Cross_SwapLE16(CA_MapHeaders[ca_mapOn]->height);
+	#endif
+
+	CK_Cross_LogMessage(CK_LOG_MSG_NORMAL, "CA_CacheMapFromASLEV: Loading map \"%s\" (\"%s\")\n", fname, CA_MapHeaders[ca_mapOn]->name);
+
+	int planeSize = CA_MapHeaders[ca_mapOn]->width * CA_MapHeaders[ca_mapOn]->height * 2;
+	int decompSize = planeSize * CA_NUMMAPPLANES;
+
+	// Decompress the data.
+	void *decompData1, *decompData;
+	MM_GetPtr((mm_ptr_t*) &decompData1, decompSize);
+	// First, LZW
+	CAL_LZWExpand(&aslevFile[32], decompData1, aslevFileLen - 32, 24);
+
+	// We can now free the file.
+	MM_FreePtr((mm_ptr_t*)&aslevFile);
+
+	MM_GetPtr((mm_ptr_t*) &decompData, decompSize);
+	CAL_RLEWExpand((int8_t*)decompData1, (int16_t*)decompData, decompSize, 0x0539);
+	MM_FreePtr((mm_ptr_t*)&decompData1);
+
+	//Load the map data
+	for (int plane = 0; plane < CA_NUMMAPPLANES; ++plane)
+	{
+		int32_t planeOffset = plane * planeSize;
+
+		MM_GetPtr((void **)&CA_mapPlanes[plane], planeSize);
+		memcpy(CA_mapPlanes[plane], (uint8_t*)decompData + planeOffset, planeSize);
+	}
+	MM_FreePtr((mm_ptr_t*)&decompData);
+}
+
 
 // CA_Startup opens the core CA datafiles
 void CA_Startup(void)

src/id_ca.h
@@ -117,6 +117,7 @@ extern void (*ca_updateCacheBox)(void);
 extern void (*ca_finishCacheBox)(void);
 
 void CA_CacheMap(int mapIndex);
+void CA_CacheMapFromASLEV(const char *fname);
 uint16_t *CA_TilePtrAtPos(int16_t x, int16_t y, int16_t plane);
 uint16_t CA_TileAtPos(int16_t x, int16_t y, int16_t plane);
 void CA_SetTileAtPos(int16_t x, int16_t y, int16_t plane, uint16_t value);

src/ck_main.c
@@ -759,6 +759,12 @@ int main(int argc, char *argv[])
 			CK_PlayDemo(atoi(argv[i + 1]));
 			Quit(0);
 		}
+		else if (!CK_Cross_strcasecmp(argv[i], "/ASLEV"))
+		{
+			us_tedLevel = true;
+			us_aslevFilename = argv[++i];
+
+		}
 
 	}
 

src/id_us.h
@@ -95,6 +95,7 @@ void US_SetPrintColour(int8_t parm);
 extern bool us_noWait;	// Debug mode enabled.
 extern bool us_tedLevel;      // Launching a level from TED
 extern int us_tedLevelNumber; // Number of level to launch from TED
+extern const char *us_aslevFilename; // Filename of an Abiather ASLEV file to play.
 
 // We need to steal these from main().
 extern const char **us_argv;

src/id_us_1.c
@@ -679,7 +679,8 @@ static const char *us_parmStrings[] = {"TEDLEVEL", "NOWAIT", "\0", NULL};
 
 bool us_noWait;	// Debug mode enabled.
 bool us_tedLevel;      // Launching a level from TED
-int us_tedLevelNumber; // Number of level to launch from TED
+int us_tedLevelNumber = 1; // Number of level to launch from TED
+const char *us_aslevFilename = NULL;	// Filename of an Abiathar ASLEV file to play.
 
 // We need to steal these from main().
 const char **us_argv;

commit 8f0ce785bfff4c2350a71fc920c75c112908da59

id_ca: Fix .aslev loading to set ca_mapOn
In early versions, this was hardcoded to 1, but instead use whatever TEDLEVEL
sets.

Fixes: 49a5f6160331 ("Abiathar '.aslev' support")

src/ck_game.c
@@ -602,7 +602,7 @@ void CK_LoadLevel(bool doCache, bool silent)
 	}
 
 	if (us_aslevFilename)
-		CA_CacheMapFromASLEV(us_aslevFilename);
+		CA_CacheMapFromASLEV(ck_gameState.currentLevel, us_aslevFilename);
 	else
 		CA_CacheMap(ck_gameState.currentLevel);
 	RF_NewMap();

src/id_ca.c
@@ -1289,7 +1289,7 @@ void CA_CacheMap(int mapIndex)
 }
 
 // Cache a map from an Abiathar .aslev file.
-void CA_CacheMapFromASLEV(const char *fname)
+void CA_CacheMapFromASLEV(int mapIndex, const char *fname)
 {
 	//Unload the previous map.
 	for (int plane = 0; plane < CA_NUMMAPPLANES; ++plane)
@@ -1300,6 +1300,8 @@ void CA_CacheMapFromASLEV(const char *fname)
 		}
 	}
 
+	ca_mapOn = mapIndex;
+
 	uint8_t *aslevFile;
 	int aslevFileLen;
 
@@ -1309,7 +1311,7 @@ void CA_CacheMapFromASLEV(const char *fname)
 	//Have we loaded this map's header?
 	if (CA_MapHeaders[ca_mapOn])
 	{
-		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Tried to override an already-loaded map (%d) with a .aslev!", ca_mapOn);
+		CK_Cross_LogMessage(CK_LOG_MSG_WARNING, "Tried to override an already-loaded map (%d) with a .aslev!\n", ca_mapOn);
 		MM_FreePtr((void **)(&CA_MapHeaders[ca_mapOn]));
 	}
 	MM_GetPtr((void **)(&CA_MapHeaders[ca_mapOn]), sizeof(CA_MapHeader));
@@ -1324,7 +1326,7 @@ void CA_CacheMapFromASLEV(const char *fname)
 	CA_MapHeaders[ca_mapOn]->height = CK_Cross_SwapLE16(CA_MapHeaders[ca_mapOn]->height);
 	#endif
 
-	CK_Cross_LogMessage(CK_LOG_MSG_NORMAL, "CA_CacheMapFromASLEV: Loading map \"%s\" (\"%s\")\n", fname, CA_MapHeaders[ca_mapOn]->name);
+	CK_Cross_LogMessage(CK_LOG_MSG_NORMAL, "CA_CacheMapFromASLEV: Loading map \"%s\" (\"%s\") as map %d.\n", fname, CA_MapHeaders[ca_mapOn]->name, ca_mapOn);
 
 	int planeSize = CA_MapHeaders[ca_mapOn]->width * CA_MapHeaders[ca_mapOn]->height * 2;
 	int decompSize = planeSize * CA_NUMMAPPLANES;

src/id_ca.h
@@ -117,7 +117,7 @@ extern void (*ca_updateCacheBox)(void);
 extern void (*ca_finishCacheBox)(void);
 
 void CA_CacheMap(int mapIndex);
-void CA_CacheMapFromASLEV(const char *fname);
+void CA_CacheMapFromASLEV(int mapIndex, const char *fname);
 uint16_t *CA_TilePtrAtPos(int16_t x, int16_t y, int16_t plane);
 uint16_t CA_TileAtPos(int16_t x, int16_t y, int16_t plane);
 void CA_SetTileAtPos(int16_t x, int16_t y, int16_t plane, uint16_t value);