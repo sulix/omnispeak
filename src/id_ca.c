sulix/omnispeak

commit 27ef8235767baecabc2508c1645d9ed243c20426

id_ca: Read map headers without packed structs
The packed struct "feature" isn't standardised in C, almost certainly
_won't_ be ever standardised, and isn't well supported by smaller or
more obscure compilers. (And the way we use macros to support different
compilers isn't perfectly extensible to, e.g., the way tcc works, so
we can't build a working version with it).

So, let's start phasing out packed structs where we can. To start off with,
we'll read map headers out one field at a time. This also lets us get rid of
some of the more bespoke endianness conversion stuff, which is nice.

src/id_ca.c
@@ -1223,19 +1223,34 @@ void CA_CacheMap(int mapIndex)
 		MM_GetPtr((void **)(&CA_MapHeaders[mapIndex]), sizeof(CA_MapHeader));
 
 		FS_SeekTo(ca_GameMaps, headerOffset);
+		printf("headeroffset = %x\n", headerOffset);
 
-		size_t mapHeaderSize = FS_Read(CA_MapHeaders[mapIndex], sizeof(CA_MapHeader), 1, ca_GameMaps);
-		if (mapHeaderSize != 1)
+		size_t entriesRead = FS_ReadInt32LE(CA_MapHeaders[mapIndex]->planeOffsets, CA_NUMMAPPLANES, ca_GameMaps);
+		if (entriesRead != CA_NUMMAPPLANES)
 			Quit("Couldn't read map header from GAMEMAPS!");
-#ifdef CK_CROSS_IS_BIGENDIAN
-		for (int plane = 0; plane < CA_NUMMAPPLANES; ++plane)
-		{
-			CA_MapHeaders[mapIndex]->planeOffsets[plane] = CK_Cross_SwapLE32(CA_MapHeaders[mapIndex]->planeOffsets[plane]);
-			CA_MapHeaders[mapIndex]->planeLengths[plane] = CK_Cross_SwapLE16(CA_MapHeaders[mapIndex]->planeLengths[plane]);
-		}
-		CA_MapHeaders[mapIndex]->width = CK_Cross_SwapLE16(CA_MapHeaders[mapIndex]->width);
-		CA_MapHeaders[mapIndex]->height = CK_Cross_SwapLE16(CA_MapHeaders[mapIndex]->height);
-#endif
+
+		entriesRead = FS_ReadInt16LE(CA_MapHeaders[mapIndex]->planeLengths, CA_NUMMAPPLANES, ca_GameMaps);
+		if (entriesRead != CA_NUMMAPPLANES)
+			Quit("Couldn't read map header from GAMEMAPS!");
+
+		entriesRead = FS_ReadInt16LE(&CA_MapHeaders[mapIndex]->width, 1, ca_GameMaps);
+		if (entriesRead != 1)
+			Quit("Couldn't read map header from GAMEMAPS!");
+
+		entriesRead = FS_ReadInt16LE(&CA_MapHeaders[mapIndex]->height, 1, ca_GameMaps);
+		if (entriesRead != 1)
+			Quit("Couldn't read map header from GAMEMAPS!");
+
+		entriesRead = FS_Read(CA_MapHeaders[mapIndex]->name, 1, 16, ca_GameMaps);
+		if (entriesRead != 16)
+			Quit("Couldn't read map header from GAMEMAPS!");
+
+		entriesRead = FS_Read(CA_MapHeaders[mapIndex]->signature, 1, 4, ca_GameMaps);
+		if (entriesRead != 4)
+			Quit("Couldn't read map header from GAMEMAPS!");
+
+
+		//size_t mapHeaderSize = FS_Read(CA_MapHeaders[mapIndex], sizeof(CA_MapHeader), 1, ca_GameMaps);
 	}
 	else
 	{

src/ck4_misc.c
@@ -129,8 +129,8 @@ void CK4_ScanInfoLayer()
 	CA_ClearLumps();
 
 	//TODO: Work out where to store current map number, etc.
-	int mapW = CA_MapHeaders[ca_mapOn]->width;
-	int mapH = CA_MapHeaders[ca_mapOn]->height;
+	int mapW = CA_GetMapWidth();
+	int mapH = CA_GetMapHeight();
 
 	for (int y = 0; y < mapH; ++y)
 	{

src/ck5_misc.c
@@ -290,8 +290,8 @@ void CK5_ScanInfoLayer()
 	CA_InitLumps();
 
 	//TODO: Work out where to store current map number, etc.
-	int mapW = CA_MapHeaders[ca_mapOn]->width;
-	int mapH = CA_MapHeaders[ca_mapOn]->height;
+	int mapW = CA_GetMapWidth();
+	int mapH = CA_GetMapHeight();
 
 	ck_gameState.ep.ck5.fusesRemaining = 0;
 

src/ck6_misc.c
@@ -88,8 +88,8 @@ void CK6_ScanInfoLayer()
 {
 	CA_ClearLumps();
 
-	int mapW = CA_MapHeaders[ca_mapOn]->width;
-	int mapH = CA_MapHeaders[ca_mapOn]->height;
+	int mapW = CA_GetMapWidth();
+	int mapH = CA_GetMapHeight();
 
 	for (int y = 0; y < mapH; ++y)
 	{

src/id_ca.h
@@ -96,14 +96,14 @@ void CA_DownLevel(void);
 #define CA_NUMMAPPLANES 3
 #define CA_NUMMAPS 100
 
-typedef CK_PACKED_STRUCT(CA_MapHeader
+typedef struct CA_MapHeader
 {
 	uint32_t planeOffsets[CA_NUMMAPPLANES];
 	uint16_t planeLengths[CA_NUMMAPPLANES];
 	uint16_t width, height;
 	char name[16];
 	char signature[4];
-}) CA_MapHeader;
+} CA_MapHeader;
 
 extern CA_MapHeader *CA_MapHeaders[CA_NUMMAPS];
 

src/id_rf.c
@@ -626,12 +626,12 @@ void RF_Shutdown()
 // TODO: More to change? Also, originally mapNum is a global variable.
 void RF_NewMap(void)
 {
-	rf_mapWidthTiles = CA_MapHeaders[ca_mapOn]->width;
-	rf_mapHeightTiles = CA_MapHeaders[ca_mapOn]->height;
+	rf_mapWidthTiles = CA_GetMapWidth();
+	rf_mapHeightTiles = CA_GetMapHeight();
 	rf_scrollXMinUnit = 0x0200; //Two-tile wide border around map
 	rf_scrollYMinUnit = 0x0200;
-	rf_scrollXMaxUnit = RF_TileToUnit(CA_MapHeaders[ca_mapOn]->width - RF_SCREEN_WIDTH_TILES - 2);
-	rf_scrollYMaxUnit = RF_TileToUnit(CA_MapHeaders[ca_mapOn]->height - RF_SCREEN_HEIGHT_TILES - 2);
+	rf_scrollXMaxUnit = RF_TileToUnit(CA_GetMapWidth() - RF_SCREEN_WIDTH_TILES - 2);
+	rf_scrollYMaxUnit = RF_TileToUnit(CA_GetMapHeight() - RF_SCREEN_HEIGHT_TILES - 2);
 
 	CK_Cross_LogMessage(CK_LOG_MSG_NORMAL, "RF_NewMap: Initialising map %d (%d×%d)\n",ca_mapOn, rf_mapWidthTiles, rf_mapHeightTiles);
 
@@ -647,9 +647,9 @@ void RF_NewMap(void)
 
 	// Set-up a two-tile wide border
 	RF_SetScrollBlock(0, 1, true);
-	RF_SetScrollBlock(0, CA_MapHeaders[ca_mapOn]->height - 2, true);
+	RF_SetScrollBlock(0, CA_GetMapHeight()- 2, true);
 	RF_SetScrollBlock(1, 0, false);
-	RF_SetScrollBlock(CA_MapHeaders[ca_mapOn]->width - 2, 0, false);
+	RF_SetScrollBlock(CA_GetMapWidth() - 2, 0, false);
 
 	SD_SetLastTimeCount(SD_GetTimeCount());
 	SD_SetSpriteSync(1);

commit 7223d6ac5501c8a065c58372f937e0c00cb160be

id_ca: Remove mistakenly-committed debug printf
Whoops, when redoing the code to load map headers, we left a debug printf()
— and some commented-out code — in. Remove it.

Fixes: 27ef8235767b ("id_ca: Read map headers without packed structs")

src/id_ca.c
@@ -1223,7 +1223,6 @@ void CA_CacheMap(int mapIndex)
 		MM_GetPtr((void **)(&CA_MapHeaders[mapIndex]), sizeof(CA_MapHeader));
 
 		FS_SeekTo(ca_GameMaps, headerOffset);
-		printf("headeroffset = %x\n", headerOffset);
 
 		size_t entriesRead = FS_ReadInt32LE(CA_MapHeaders[mapIndex]->planeOffsets, CA_NUMMAPPLANES, ca_GameMaps);
 		if (entriesRead != CA_NUMMAPPLANES)
@@ -1248,9 +1247,6 @@ void CA_CacheMap(int mapIndex)
 		entriesRead = FS_Read(CA_MapHeaders[mapIndex]->signature, 1, 4, ca_GameMaps);
 		if (entriesRead != 4)
 			Quit("Couldn't read map header from GAMEMAPS!");
-
-
-		//size_t mapHeaderSize = FS_Read(CA_MapHeaders[mapIndex], sizeof(CA_MapHeader), 1, ca_GameMaps);
 	}
 	else
 	{