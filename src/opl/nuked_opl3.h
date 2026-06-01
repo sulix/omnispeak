af8b898c84bd0f8e89bf74489ba559ee22fabf17
Tony Gies <tgies@tgies.net>
2026-06-01 17:17:12 -0500

    nukedopl3: Replace Nuked-OPL3 with Nuked-OPL3-fast
    Replace Nuked-OPL3 with a performance fork with identical output to
    upstream Nuked-OPL3. Also bring in some minor envelope generator
    accuracy fixes from upstream.
    
    [Edit: Update AUTHORS as well. --David ]

--- src/opl/nuked_opl3.h
+++ src/opl/nuked_opl3.h
@@ -1,5 +1,8 @@
+
 /* Nuked OPL3
+ *
  * Copyright (C) 2013-2020 Nuke.YKT
+ * Copyright (C) 2026 Tony Gies (Nuked-OPL3-fast modifications)
  *
  * This file is part of Nuked OPL3.
  *
@@ -15,7 +18,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Nuked OPL3. If not, see <https://www.gnu.org/licenses/>.
-
+ *
  *  Nuked OPL3 emulator.
  *  Thanks:
  *      MAME Development Team(Jarek Burczynski, Tatsuyuki Satoh):
@@ -27,7 +30,21 @@
  *      siliconpr0n.org(John McMaster, digshadow):
  *          YMF262 and VRC VII decaps and die shots.
  *
- * version: 1.8
+ * Upstream version: 1.8 (commit cfedb09)
+ * Fork version:    1.8-fast.1
+ * Fork home:       https://github.com/tgies/Nuked-OPL3-fast
+ *
+ * Nuked-OPL3-fast is a bit-exact performance-optimized fork of Nuked-OPL3.
+ * Audio output is identical to upstream for the same register stream.
+ *
+ * Modifications vs. upstream visible in this header:
+ *
+ *   - Added cached fields to opl3_slot: eg_tl_ksl, eg_ks, pg_inc,
+ *     eg_rate_hi[4], eg_rate_lo[4], slot_num.
+ *   - Added out_cnt to opl3_channel for mix-loop active-slot tracking.
+ *   - Reordered opl3_slot to put hot per-sample fields first; struct size
+ *     shrank from 96 to 88 bytes.
+ *   - Removed unused legacy fields (eg_inc, eg_rate) from opl3_slot.
  */
 
 #ifndef OPL_OPL3_H
@@ -53,40 +70,48 @@ typedef struct _opl3_chip opl3_chip;
 struct _opl3_slot {
     opl3_channel *channel;
     opl3_chip *chip;
+    int16_t *mod;
+    uint8_t *trem;
+    uint32_t pg_reset;
+    uint32_t pg_phase;
+    uint32_t pg_inc;
     int16_t out;
     int16_t fbmod;
-    int16_t *mod;
     int16_t prout;
     uint16_t eg_rout;
     uint16_t eg_out;
-    uint8_t eg_inc;
+    /* Cached (reg_tl << 2) + (eg_ksl >> kslshift[reg_ksl]); maintained by
+     * OPL3_EnvelopeUpdateKSL whenever any of those inputs change. Hoists
+     * a load + lookup + shift out of the per-sample envelope hot path. */
+    uint16_t eg_tl_ksl;
+    uint16_t pg_phase_out;
+    uint8_t key;
     uint8_t eg_gen;
-    uint8_t eg_rate;
-    uint8_t eg_ksl;
-    uint8_t *trem;
     uint8_t reg_vib;
+    uint8_t reg_mult;
+    uint8_t reg_wf;
+    uint8_t slot_num;
+    uint8_t eg_ksl;
+    uint8_t eg_ks;
     uint8_t reg_type;
     uint8_t reg_ksr;
-    uint8_t reg_mult;
     uint8_t reg_ksl;
     uint8_t reg_tl;
     uint8_t reg_ar;
     uint8_t reg_dr;
     uint8_t reg_sl;
     uint8_t reg_rr;
-    uint8_t reg_wf;
-    uint8_t key;
-    uint32_t pg_reset;
-    uint32_t pg_phase;
-    uint16_t pg_phase_out;
-    uint8_t slot_num;
+    uint8_t eg_rates[4];
+    uint8_t eg_rate_hi[4];
+    uint8_t eg_rate_lo[4];
 };
 
 struct _opl3_channel {
-    opl3_slot *slots[2];
+    opl3_slot *slotz[2];/*Don't use "slots" keyword to avoid conflict with Qt applications*/
     opl3_channel *pair;
     opl3_chip *chip;
     int16_t *out[4];
+    uint8_t out_cnt;
 
 #if OPL_ENABLE_STEREOEXT
     int32_t leftpan;
@@ -101,6 +126,7 @@ struct _opl3_channel {
     uint8_t alg;
     uint8_t ksv;
     uint16_t cha, chb;
+    uint16_t chc, chd;
     uint8_t ch_num;
 };
 
@@ -118,6 +144,7 @@ struct _opl3_chip {
     uint8_t eg_timerrem;
     uint8_t eg_state;
     uint8_t eg_add;
+    uint8_t eg_timer_lo;
     uint8_t newm;
     uint8_t nts;
     uint8_t rhy;
@@ -126,9 +153,10 @@ struct _opl3_chip {
     uint8_t tremolo;
     uint8_t tremolopos;
     uint8_t tremoloshift;
+    uint8_t tremolo_dirty;
     uint32_t noise;
     int16_t zeromod;
-    int32_t mixbuff[2];
+    int32_t mixbuff[4];
     uint8_t rm_hh_bit2;
     uint8_t rm_hh_bit3;
     uint8_t rm_hh_bit7;
@@ -143,8 +171,8 @@ struct _opl3_chip {
     /* OPL3L */
     int32_t rateratio;
     int32_t samplecnt;
-    int16_t oldsamples[2];
-    int16_t samples[2];
+    int16_t oldsamples[4];
+    int16_t samples[4];
 
     uint64_t writebuf_samplecnt;
     uint32_t writebuf_cur;
@@ -159,6 +187,10 @@ void OPL3_Reset(opl3_chip *chip, uint32_t samplerate);
 void OPL3_WriteReg(opl3_chip *chip, uint16_t reg, uint8_t v);
 void OPL3_WriteRegBuffered(opl3_chip *chip, uint16_t reg, uint8_t v);
 void OPL3_GenerateStream(opl3_chip *chip, int16_t *sndptr, uint32_t numsamples);
+
+void OPL3_Generate4Ch(opl3_chip *chip, int16_t *buf4);
+void OPL3_Generate4ChResampled(opl3_chip *chip, int16_t *buf4);
+void OPL3_Generate4ChStream(opl3_chip *chip, int16_t *sndptr1, int16_t *sndptr2, uint32_t numsamples);
 
 #ifdef __cplusplus
 }
